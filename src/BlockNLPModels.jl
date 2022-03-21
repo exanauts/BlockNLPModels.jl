07ecc21d0b15c7e13a00e2bf019d16e4327d49e9
grishabh147 <67451073+grishabh147@users.noreply.github.com>
2022-03-21 15:39:16 -0500

    Create module

--- src/BlockNLPModels.jl
+++ src/BlockNLPModels.jl
@@ -1,28 +1,17 @@
-using JuMP, Random, NLPModels, NLPModelsJuMP, Ipopt, SolverCore, NLPModelsIpopt
+module BlockNLPModels
+
+using NLPModels
+using NLPModelsJuMP
+using Ipopt
+using SolverCore
+using NLPModelsIpopt
+
 
 include("blk_NLP.jl")
 include("dd.jl")
 include("Block_Solver.jl")
 include("Full_Space.jl")
 
-# Generate a test problem
-rng= MersenneTwister(1234)
-B = 3 # number of blocks
+export BlockNLPModel, dual_decomposition, FullSpace 
 
-blk = []
-T = rand(rng, 1:5, B)
-for i in 1:B
-    model = Model()
-    @variable(model, x >= 0)
-    @objective(model, Min, (x - T[i])^2)
-    nlp = MathOptNLPModel(model)
-    push!(blk, nlp)
 end
-A = ones(Float64, B)
-b = rand(rng, 100*B:500*B)/100
-bm = BlockNLPModel(blk, vcat(A, b), λ0 = 0.0)
-
-# Solve using dual decomposition
-dual_decomposition(bm)
-# Solve the full-space problem with Ipopt
-stats = ipopt(FullSpace(bm))
---
+++ src/testproblem.jl
@@ -0,0 +1,23 @@
+using Random, JuMP
+import BlockNLPModels
+# Generate a test problem
+rng= MersenneTwister(1234)
+B = 3 # number of blocks
+
+blk = []
+T = rand(rng, 1:5, B)
+for i in 1:B
+    model = Model()
+    @variable(model, x >= 0)
+    @objective(model, Min, (x - T[i])^2)
+    nlp = MathOptNLPModel(model)
+    push!(blk, nlp)
+end
+A = ones(Float64, B)
+b = rand(rng, 100*B:500*B)/100
+bm = BlockNLPModel(blk, vcat(A, b), λ0 = 0.0)
+
+# Solve using dual decomposition
+dual_decomposition(bm)
+# Solve the full-space problem with Ipopt
+stats = ipopt(FullSpace(bm))
