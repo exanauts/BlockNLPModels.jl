84c35e2df2a82a6775fcdd15ba7d27426d4be62e
grishabh147 <67451073+grishabh147@users.noreply.github.com>
2022-04-08 13:45:31 -0500

    made linking_constraints modular

--- src/BlockNLPModels.jl
+++ src/BlockNLPModels.jl
@@ -12,17 +12,17 @@ export add_block
 export add_links
 
 abstract type AbstractBlockNLPModel end
-abstract type AbstractLinkConstraint end
-
-# To Do: Either sort the dictionaries or use a different representation
+abstract type AbstractLinearLinkConstraint end
+abstract type AbstractNLLinkConstraint end
 
 # To keep a count of different objects attached to the model
 mutable struct BlockNLP_Counters
   block_counter::Int
   link_counter::Int
   var_counter::Int
+  con_counter::Int
   function BlockNLP_Counters()
-      return new(0,0,0)
+      return new(0,0,0,0)
   end    
 end
 
@@ -32,86 +32,95 @@ mutable struct AbstractBlockModel{T, S} <: AbstractNLPModel{T, S}
   problem_block::AbstractNLPModel
   idx::Int
   var_idx::UnitRange{Int} # Indices of variables wrt the full space model
+  con_idx::UnitRange{Int} # Indices of constraints wrt the full space model
   linking_constraints::Vector{Int} # ID of linking constraints that connect this block with other blocks 
 end
 
-mutable struct LinkConstraint <: AbstractLinkConstraint
-  linking_matrices::Vector{SparseMatrixCSC{Float64,Int}} # linking_matrices[i] will give out the corresponsing A_i
+mutable struct LinearLinkConstraint <: AbstractLinearLinkConstraint
+  linking_blocks::Vector{SparseMatrixCSC{Float64,Int}} # linking_matrices[i] will give out the corresponsing A_i
+  idx::UnitRange{Int}
   link_map::Dict{Int, Vector{Int}} # constraint => blocks, i.e., which constraint connects which blocks
-  RHS_vector::SparseVector{Float64, Int}
-  function LinkConstraint()
-      return new(
-          Vector{SparseMatrixCSC{Float64,Int}}(), 
-          Dict{Int, Vector{Int}}(), 
-          SparseVector{Float64, Int}(0, Vector{Int}(), Vector{Float64}())
-          )
-  end 
+  RHS_vector::Vector{Float64}
+end
+
+mutable struct NLLinkConstraint <: AbstractNLLinkConstraint
 end
 
 mutable struct BlockNLPModel <: AbstractBlockNLPModel
   problem_size::BlockNLP_Counters
   blocks::Vector{AbstractBlockModel}
-  linking_constraints::AbstractLinkConstraint
+  linking_constraints::Vector{Union{AbstractLinearLinkConstraint, AbstractNLLinkConstraint}}
   function BlockNLPModel()
     return new(
       BlockNLP_Counters(),
       Vector{AbstractBlockModel}(),
-      LinkConstraint()
+      Vector{Union{AbstractLinearLinkConstraint, AbstractNLLinkConstraint}}()
     )
   end
 end
 
 function add_block(block_nlp::AbstractBlockNLPModel, nlp::AbstractNLPModel)
   block_nlp.problem_size.block_counter += 1
+
   temp = deepcopy(block_nlp.problem_size.var_counter)+1
   block_nlp.problem_size.var_counter += nlp.meta.nvar
   var_idx = temp:block_nlp.problem_size.var_counter
-  meta = nlp.meta
-  push!(block_nlp.blocks, AbstractBlockModel(meta, Counters(), 
-  nlp, block_nlp.problem_size.block_counter, var_idx, Vector{Int}()))
+
+  temp = deepcopy(block_nlp.problem_size.con_counter) # number of constraints in a block can be zero
+  block_nlp.problem_size.con_counter += nlp.meta.ncon
+  con_idx = temp:block_nlp.problem_size.con_counter
+  push!(block_nlp.blocks, AbstractBlockModel(nlp.meta, Counters(), 
+  nlp, block_nlp.problem_size.block_counter, var_idx, con_idx, Vector{Int}()))
 end
 
 # This function must be called after blocks have been added
 function add_links(block_nlp::AbstractBlockNLPModel, n_constraints::Int, 
-  links::Dict{Int, SparseMatrixCSC{Float64, Int64}}, constants::SparseVector{Float64, Int})
-  # Add n_constraints rows to the linking matrices
-  for i in 1:block_nlp.problem_size.block_counter
-      if block_nlp.problem_size.link_counter == 0
-          push!(block_nlp.linking_constraints.linking_matrices, 
-          spzeros(n_constraints, block_nlp.blocks[i].meta.nvar))
-      else
-          block_nlp.linking_constraints.linking_matrices[i] = 
-          vcat(block_nlp.linking_constraints.linking_matrices[i], 
-          spzeros(n_constraints, block_nlp.blocks[i].meta.nvar))
-      end
-  end
-  # Add n_constraint rows to the RHS constant vector
-  if block_nlp.problem_size.link_counter == 0
-      block_nlp.linking_constraints.RHS_vector = constants
-  else
-      block_nlp.linking_constraints.RHS_vector = 
-      vcat(block_nlp.linking_constraints.RHS_vector, constants)
-  end
+  links::Union{Dict{Int, Array{Float64, 2}}, Dict{Int, Vector{Float64}}}, 
+  constants::Union{AbstractVector, Float64})
+  
+  linking_blocks = Vector{SparseMatrixCSC{Float64, Int}}(undef, block_nlp.problem_size.block_counter)
+  RHS_vector = Vector{Float64}(undef, n_constraints)
 
-  # check if all dictionary elements are of appropriate sizes
-  for block_idx in keys(links)
+  # Initialize linking_blocks with empty matrices
+  for i in 1:block_nlp.problem_size.block_counter
+    linking_blocks[i] = spzeros(n_constraints, block_nlp.blocks[i].meta.nvar)
+  end
+  
+  # check if everything is of appropriate size and add to linking_blocks
+  @assert length(constants) == n_constraints
+  if n_constraints == 1
+    RHS_vector[1] = constants
+    for block_idx in keys(links)
+      @assert length(links[block_idx]) == block_nlp.blocks[block_idx].meta.nvar
+      linking_blocks[block_idx][1, :] = sparse(links[block_idx])  
+    end
+  else
+    RHS_vector = constants
+    for block_idx in keys(links)
       @assert size(links[block_idx])[1] == n_constraints
       @assert size(links[block_idx])[2] == block_nlp.blocks[block_idx].meta.nvar
+      linking_blocks[block_idx] = sparse(links[block_idx])
+    end
   end
-
-  # Parse the constraints one-by-one
+  
+  # Prepare other information
+  link_map = Dict{Int, Vector{Int}}()
+  link_con_idx = Vector{Int}()
+  # Parse the constraints one-by-one to collect this information
+  temp = block_nlp.problem_size.link_counter+1
   for con in 1:n_constraints
       block_nlp.problem_size.link_counter += 1
-      block_nlp.linking_constraints.link_map[block_nlp.problem_size.link_counter] = Vector{Int}()
+      link_map[block_nlp.problem_size.link_counter] = Vector{Int}()
       for block_idx in keys(links)
-          if links[block_idx][con, :] != spzeros(block_nlp.blocks[block_idx].meta.nvar)
+          if linking_blocks[block_idx][con, :] != spzeros(block_nlp.blocks[block_idx].meta.nvar)
               push!(block_nlp.blocks[block_idx].linking_constraints, block_nlp.problem_size.link_counter)
-              block_nlp.linking_constraints.linking_matrices[block_idx][block_nlp.problem_size.link_counter, :] = 
-              links[block_idx][con, :]
-              push!(block_nlp.linking_constraints.link_map[block_nlp.problem_size.link_counter], block_idx)
+              push!(link_map[block_nlp.problem_size.link_counter], block_idx)
           end
       end
   end
+  link_con_idx = temp:block_nlp.problem_size.link_counter
+  lin_link_con = LinearLinkConstraint(linking_blocks, link_con_idx, link_map, RHS_vector)
+  push!(block_nlp.linking_constraints, lin_link_con)
 end
 
 include("dualized_block.jl")
