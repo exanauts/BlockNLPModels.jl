24dc87ad9d0dc1531d983adba97c3f996cb06d29
grishabh147 <67451073+grishabh147@users.noreply.github.com>
2022-04-09 17:04:54 -0500

    added docstrings

--- src/utils.jl
+++ src/utils.jl
@@ -1,17 +1,39 @@
+"""
+  get_blockmatrix(
+    m::AbstractBlockNLPModel 
+  )
+
+Returns the concatenated block matrix ``A = [A_1, \\ldots, A_B]``
+
+# Arguments
+
+- `m::AbstractBlockNLPModel`: BlockNLPModel whose block matrix is to be extracted.
+"""
 function get_blockmatrix(m::AbstractBlockNLPModel)
     nb = m.problem_size.block_counter
     A = spzeros(m.problem_size.link_counter, m.problem_size.var_counter)
     for i in 1:nb
         for j in 1:length(m.linking_constraints)
-            if m.linking_constraints[j]::AbstractLinearLinkConstraint
+            # if m.linking_constraints[j] isa AbstractLinearLinkConstraint
                 A[m.linking_constraints[j].idx, m.blocks[i].var_idx] = 
                 m.linking_constraints[j].linking_blocks[i]
-            end
+            # end
         end
     end
     return A
 end
 
+"""
+  get_matrixblocks(
+    m::AbstractBlockNLPModel 
+  )
+
+Returns a vector of block matrices `[A_1, \\ldots, A_B]``
+
+# Arguments
+
+- `m::AbstractBlockNLPModel`: name of the BlockNLPModel whose block matrices are to be extracted.
+"""
 function get_matrixblocks(m::AbstractBlockNLPModel)
     nb = m.problem_size.block_counter
     A = Vector{SparseMatrixCSC{Float64, Int}}(undef, nb)
@@ -25,14 +47,36 @@ function get_matrixblocks(m::AbstractBlockNLPModel)
     return A
 end
 
+"""
+    get_RHSvector(
+        m::AbstractBlockNLPModel 
+    )
+
+Returns the concatenated RHS vector for all the linking constraints ``b``.
+
+# Arguments
+
+- `m::AbstractBlockNLPModel`: name of the BlockNLPModel whose RHS vector is to be extracted.
+"""
 function get_RHSvector(m::AbstractBlockNLPModel)
     b = zeros(m.problem_size.link_counter)
     for j in 1:length(m.linking_constraints)
-        b[m.linking_constraints[j].idx] = b.linking_constraints[j].RHS_vector
+        b[m.linking_constraints[j].idx] = m.linking_constraints[j].RHS_vector
     end
     return b
 end
 
+"""
+    n_tot_con(
+        m::AbstractBlockNLPModel 
+    )
+
+Returns the total number of constraints in a BlockNLPModel.
+
+# Arguments
+
+- `m::AbstractBlockNLPModel`: name of the BlockNLPModel.
+"""
 function n_tot_con(m::AbstractBlockNLPModel)
     return m.problem_size.link_counter + m.problem_size.con_counter 
 end
